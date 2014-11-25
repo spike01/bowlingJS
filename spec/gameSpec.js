f0bd452ffeea856c745bcce03ac7434cba86e3d6
Spike Nox <spike01@gmail.com>
2014-11-25 17:13:57 +0000

    Third test - one spare

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,22 +1,33 @@
-describe("Game", function() {
+describe("Game", function(){
+
   var game;
 
-  beforeEach(function() {
+  beforeEach(function(){
     game = new Game();
   });
 
-  it("Gutter game", function() {
-    for(var i = 0; i < 20; i++){
-      game.roll(0);
+  function rollMany(rolls, pins){
+    for(var i = 0; i < rolls; i++){
+      game.roll(pins);
     }
+  };
+
+  it("Gutter game", function(){
+    rollMany(20, 0); 
     expect(game.score()).toBe(0);
   });
 
-  it("All ones", function() {
-    for(var i = 0; i < 20; i++){
-      game.roll(1);
-    }
+  it("All ones", function(){
+    rollMany(20, 1);
     expect(game.score()).toBe(20);
+  });
+
+  it("One spare", function(){
+    game.roll(5);
+    game.roll(5); //spare
+    game.roll(3);
+    rollMany(17, 0);
+    expect(game.score()).toBe(16);
   });
 
 });
