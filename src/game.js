3e5991a2597a360d63edbdf9cecebf2711f7de90
Spike Nox <spike01@gmail.com>
2015-06-11 17:16:59 +0100

    Refactors for maximum read-like-Englishness

--- src/game.js
+++ src/game.js
@@ -13,37 +13,56 @@ Game.prototype.roll = function(pins){
 Game.prototype.score = function(){
   var score = 0;
   var frameIndex = 0;
+
   for(var frame = 0; frame < 10; frame++){
-    if(this.isStrike(frameIndex)){
-      score += 10 + this.strikeBonus(frameIndex);
-        frameIndex ++;
-    } else if(this.isSpare(frameIndex)){
-      score += 10 + this.spareBonus(frameIndex);
-      frameIndex += 2
+    if(this._isStrike(frameIndex)){
+      score += 10 + this._strikeBonus(frameIndex);
+      nextRoll();
+    } else if(this._isSpare(frameIndex)){
+      score += 10 + this._spareBonus(frameIndex);
+      nextFrame();
     } else {
-      score += this.sumOfBallsInFrame(frameIndex); 
-      frameIndex += 2;
+      score += this._sumOfBallsInFrame(frameIndex); 
+      nextFrame();
     }
   }
+
   return score;
+
+  function nextFrame(){
+    frameIndex += 2
+  }
+  function nextRoll(){
+    frameIndex += 1
+  }
 };
 
-Game.prototype.isStrike = function(frameIndex){
+//private
+
+Game.prototype._isStrike = function(frameIndex){
   return this.rolls[frameIndex] === 10;
 };
 
-Game.prototype.isSpare = function(frameIndex){
-  return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+Game.prototype._isSpare = function(frameIndex){
+  return this.rolls[frameIndex] + this._nextRollScore(frameIndex) === 10;
 };
 
-Game.prototype.strikeBonus = function(frameIndex){
-  return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+Game.prototype._strikeBonus = function(frameIndex){
+  return this._nextRollScore(frameIndex) + this._nextNextRollScore(frameIndex);
 };
 
-Game.prototype.spareBonus = function(frameIndex){
-  return this.rolls[frameIndex + 2];
+Game.prototype._spareBonus = function(frameIndex){
+  return this._nextNextRollScore(frameIndex);
 };
 
-Game.prototype.sumOfBallsInFrame = function(frameIndex){
-  return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+Game.prototype._sumOfBallsInFrame = function(frameIndex){
+  return this.rolls[frameIndex] + this._nextRollScore(frameIndex);
+};
+
+Game.prototype._nextRollScore = function(frameIndex){
+  return this.rolls[frameIndex + 1]
+};
+
+Game.prototype._nextNextRollScore = function(frameIndex){
+  return this.rolls[frameIndex + 2]
 };
